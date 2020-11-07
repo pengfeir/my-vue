7e49d15942897c3b4d1771fa96eb8eca9a2c8abe
任鹏飞 <992430303@qq.com>
2020-11-07 22:51:36 +0800

    data数组处理

---
+++ src/observer/array.js
@@ -0,0 +1,24 @@
+let oldArrayProtoMethods = Array.prototype;
+export let arrayMethods = Object.create(oldArrayProtoMethods);
+let methods = ["push", "pop", "splice", "shift", "unshift", "reverse", "sort"];
+methods.forEach((method) => {
+  arrayMethods[method] = function (...args) {
+    const result = oldArrayProtoMethods[method].apply(this, args);
+    let inserted;
+    let ob = this.__ob__;
+    switch (method) {
+      //a:[1,2]a.push({name:1}) 数组追加的时候可能是对象，应该再次被劫持
+      case "push":
+      case "unshift":
+        inserted = args;
+        break;
+      case "splice":
+        inserted = args.slice(2);
+        break;
+      default:
+        break;
+    }
+    if (inserted) ob.observeArray(args);
+    return result;
+  };
+});
--- src/observer/index.js
+++ src/observer/index.js
@@ -1,11 +1,32 @@
+import { arrayMethods } from "./array";
+
 class Observer {
-  constructor(data) {
-    this.walk(data);
+  constructor(value) {
+    Object.defineProperty(value, "__ob__", {
+      enumerable: false, //不能枚举，不能循环出来，防止遍历value的时候被遍历出来
+      configurable: false,
+      value: this,
+    });
+    //判断是否数组，数组的话不添加拦截，因为数组太长的话会导致性能很差，
+    // 一般操作数组shift,push,slice,所以拦截这些方法
+    if (Array.isArray(value)) {
+      //函数劫持，切片编程
+      value.__proto__ = arrayMethods;
+      //解决数组里面就有对象没加上get,set这种情况b: [1, { a: 1 }],a上就没有get,set
+      this.observeArray(value);
+    } else {
+      this.walk(value);
+    }
   }
-  walk(data) {
-    let keys = Object.keys(data);
+  observeArray(value) {
+    value.forEach((item) => {
+      observe(item);
+    });
+  }
+  walk(value) {
+    let keys = Object.keys(value);
     keys.forEach((key) => {
-      defineReactive(data, key, data[key]);
+      defineReactive(value, key, value[key]);
     });
   }
 }
@@ -13,11 +34,11 @@ function defineReactive(data, key, value) {
   observe(value); //如果值是对象继续监测
   Object.defineProperty(data, key, {
     get() {
-      console.log("用户取值了", data, key, value, Date.now());
+      console.log("用户取值了", data, key, value);
       return value;
     },
     set(newValue) {
-      console.log("设置值了", data, key, newValue, Date.now());
+      console.log("设置值了", data, key, newValue);
       if (newValue === value) return;
       observe(newValue); //如果用户把值改成对象继续监控
       value = newValue;
@@ -28,5 +49,6 @@ export function observe(data) {
   if (typeof data !== "object" && data !== null) {
     return;
   }
+  if (data.__ob__) return;
   return new Observer(data);
 }
